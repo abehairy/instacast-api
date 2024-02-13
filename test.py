4de5c2c6fd387e5e7e0cd978e1baff26b7321bc3
Ahmed Behairy <ahmed.behairy@everestminds.com>
2024-02-13 14:13:00 +0400

    test

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
+import redis
+import ai_host
 from db import podcasts
 
 podcast = next(
     (podcast for podcast in podcasts if podcast['id'] == 'innerview'), None)
-text = podcast['introPrompt']
+text = podcast['systemPrompt']
 print(text)
+
+
+# # Connect to Redis
+# r = redis.Redis(host='localhost', port=6379, db=0)
+
+# Set a key
+response = ai_host.chat(
+    text, '32f9897c-9cfc-4c8d-ab4b-a714334f1350', 'love my work')
+
+print(response)
